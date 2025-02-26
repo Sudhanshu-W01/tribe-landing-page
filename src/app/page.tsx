ebc18a86e52a346b9c18207652ffe20937355c7e
Sudhanshu-W01 <sudhanshu.sharma@thewasserstoff.com>
2025-02-26 15:54:17 +0530

    tribe add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,6 @@
-import Benefits from "@/components/Benefits";
-import Community from "@/components/Community";
-import Footer from "@/components/Footer";
-// import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import HeroSection from "@/components/HeroSection";
-import Slider from "@/components/Slider";
-import VisionSection from "@/components/VisionSection";
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <div className="max-w-[1440px] min-w-[380px] w-full h-fit">
-      <div className="flex flex-col w-full h-full px-8 gap-8">
-        <Header/>
-        <HeroSection/>
-        <Slider/>
-        <Community/>
-        <VisionSection/>
-        <Benefits/>
-      </div>
-      <Footer/>
-    </div>
-  );
+  redirect('/tribes');
+  return null;
 }
