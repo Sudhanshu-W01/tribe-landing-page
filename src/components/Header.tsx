3ebd864a984ca6ac8ede12a7b963523ab3130276
Tribes <sidharts894@gmail.com>
2025-08-05 17:28:20 +0530

    join button changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -62,7 +62,7 @@ const Header: React.FC = () => {
 
       <button
         onClick={() => (window.location.href = "https://apptribes.astrix.live")}
-        className="laptop:hidden bg-[#B0E681] text-black font-mulish text-sm font-semibold px-6 py-1 rounded-full cursor-pointer active:bg-[#B0E681]/80"
+        className="laptop:hidden bg-[#B0E681] text-black font-mulish text-sm font-semibold px-8 py-1 rounded-full cursor-pointer active:bg-[#B0E681]/80"
       >
         Join Now
       </button>
@@ -152,7 +152,7 @@ const Header: React.FC = () => {
         <JoinWaitlistModal isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
         <button
           onClick={() => (window.location.href = "https://apptribes.astrix.live")}
-          className="hidden laptop:block bg-[#b0e681] text-black font-mulish text-lg font-semibold px-8 py-2.5 rounded-full cursor-pointer hover:bg-[#9ede66] hover:scale-105 transition-all duration-300"
+          className="hidden laptop:block bg-[#b0e681] text-black font-mulish text-lg font-semibold px-10 py-2.5 rounded-full cursor-pointer hover:bg-[#9ede66] hover:scale-105 transition-all duration-300"
         >
           Join Now
         </button>
